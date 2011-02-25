3de9f8e2e378ca71cafd35bde41e5366c7f33433
Joenio Costa <joenio@colivre.coop.br>
2011-02-25 19:06:08 -0300

    Using SQL instead of Rails Models on migration

--- db/migrate/20110222204041_set_content_type_for_some_uploaded_files.rb
+++ db/migrate/20110222204041_set_content_type_for_some_uploaded_files.rb
@@ -1,9 +1,23 @@
 class SetContentTypeForSomeUploadedFiles < ActiveRecord::Migration
   def self.up
-    select_all("SELECT id FROM articles WHERE content_type IS NULL AND type = 'UploadedFile'").each do |row|
-      file = UploadedFile.find(row["id"])
-      file.content_type = `file -ib #{file.full_filename}`.gsub(/;.+/, '')
-      file.save!
+    ids = select_all("SELECT id FROM articles WHERE content_type IS NULL and filename IS NULL AND type = 'UploadedFile'").map{|row| row["id"]}
+    ids.each do |id|
+      file = UploadedFile.find(id)
+      filename = File.join('public', 'articles/' + ("%08d" % file.id).gsub(/^(\d{4})/, '\1/'), file.name)
+      print "#{filename}: "
+      full_filename = File.join(RAILS_ROOT, filename)
+      if File.exists?(full_filename)
+        print 'found, fixing... '
+        content_type = `file -ib '#{full_filename}'`.gsub(/;.+|\s.+|\n/, '')
+        if content_type =~ /^image/
+          update(ActiveRecord::Base.sanitize_sql(["UPDATE articles SET content_type=?, filename=?, is_image=? WHERE id=?", content_type, file.name, true, id]))
+        else
+          update(ActiveRecord::Base.sanitize_sql(["UPDATE articles SET content_type=?, filename=? WHERE id=?", content_type, file.name, id]))
+        end
+        say "ok"
+      else
+        say "not found"
+      end
     end
   end
 
