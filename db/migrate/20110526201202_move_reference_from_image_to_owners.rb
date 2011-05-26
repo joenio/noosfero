b7106db6283b8925d98f81ea35c0d75cf06fcb89
Joenio Costa <joenio@colivre.coop.br>
2011-05-26 17:40:46 -0300

    Adding migration

---
+++ db/migrate/20110526201202_move_reference_from_image_to_owners.rb
@@ -0,0 +1,20 @@
+class MoveReferenceFromImageToOwners < ActiveRecord::Migration
+  def self.up
+    #%w[ profiles categories products tasks ].each do |table|
+    #  add_column table, :image_id, :integer
+    #end
+    update("update profiles set profiles.image_id = images.id from profiles inner join images on profiles.id = images.owner_id")
+    remove_colum :images, :owner_id
+    remove_colum :images, :owner_type
+  end
+
+  def self.down
+    add_colum :images, :owner_id, :ineteger
+    add_colum :images, :owner_type, :integer
+    # TODO
+    #update("update images inner join images on profiles.id = images.owner_id and profiles.type = images.owner_type set images.image_id = images.id")
+    %w[ profiles categories products tasks ].each do |table|
+      remove_column table, :image_id
+    end
+  end
+end
