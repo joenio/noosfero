eb00eb72c37b143de7a3a4cb5a285dd09456824a
Rodrigo Souto <rodrigo@colivre.coop.br>
2011-08-01 18:10:25 -0300

    [bsc-plugin-2] Fixing products extension tests

--- plugins/bsc/test/unit/bsc_plugin/product_test.rb
+++ plugins/bsc/test/unit/bsc_plugin/product_test.rb
@@ -1,7 +1,5 @@
 require File.dirname(__FILE__) + '/../../../../../test/test_helper'
 require File.dirname(__FILE__) + '/../../../../../app/models/uploaded_file'
-require File.dirname(__FILE__) + '/../../../../../app/models/product'
-require File.dirname(__FILE__) + '/../../../lib/ext/product'
 
 class ProductTest < Test::Unit::TestCase
   VALID_CNPJ = '94.132.024/0001-48'
