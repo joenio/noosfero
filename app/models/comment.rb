763526b88aa176761ddf0d758270c52b7c26383a
Rodrigo Souto <rodrigo@colivre.coop.br>
2011-09-22 11:15:10 -0300

    [abuse-report] Adding reported version to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -100,6 +100,12 @@ class Comment < ActiveRecord::Base
     root
   end
 
+  include ApplicationHelper
+  def reported_version(options = {})
+    comment = self
+    lambda { render_to_string(:partial => 'shared/reported_versions/comment', :locals => {:comment => comment}) }
+  end
+
   def to_html(option={})
     body || ''
   end
