87bf7f0d973a89399801d78a37b3b71526c4fa9d
Joenio Costa <joenio@colivre.coop.br>
2011-11-14 12:59:09 -0200

    dont sent emails on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,5 +17,6 @@ config.action_view.cache_template_loading            = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+config.action_mailer.delivery_method = :test
 
 config.cache_store = :mem_cache_store, "localhost"
