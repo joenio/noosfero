ae8333935373328c64d29c98f02e8485874f13cc
Joenio Costa <joenio@colivre.coop.br>
2011-11-30 14:45:49 -0200

    fixing checking message translations

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -4,4 +4,12 @@ module AccountHelper
     'available unavailable valid invalid checking'
   end
 
+  def checking_message(key)
+    case key
+    when :url
+      _('Checking availability of login name...')
+    when :email
+      _('Checking if e-mail address is already taken...')
+    end
+  end
 end
